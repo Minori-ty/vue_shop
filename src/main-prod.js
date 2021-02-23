032cdeea8af8b0e55a23786fbeed49c8ba1cf61b
许泽鸿 <1229248506@qq.com>
2021-02-24 00:11:40 +0800

    通过chainWebpack自定义打包入口

--- src/main-prod.js
+++ src/main-prod.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-import './assets/css/global.css'
+// 导入字体图标
 import './assets/fonts/iconfont.css'
+// 导入全局样式表
+import './assets/css/global.css'
 
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
@@ -13,20 +15,20 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 设置请求拦截器
 axios.interceptors.request.use(config => {
-  NProgress.start()
-  config.headers.Authorization = window.sessionStorage.getItem('token')
-  return config
+    NProgress.start()
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    return config
 })
 
 axios.interceptors.response.use(config => {
-  NProgress.done()
-  return config
+    NProgress.done()
+    return config
 })
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
