60386d2e7558805f03e4da8a781b635e0cc9b00f
许泽鸿 <1229248506@qq.com>
2021-02-24 11:44:16 +0800

    通过CDN优化element-ui

--- src/main-prod.js
+++ src/main-prod.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+// import './plugins/element.js'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
